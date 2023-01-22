59a497b3e492095c4905a1bb4bc8776b0451c4ae
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-01-22 12:53:08 +0000

    Update

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <NavBar />
     </div>
   );
 }
---
+++ news-app/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+function NavBar(){
+    return(
+        <div className="n-wrapper">
+            <div className="n-left">
+
+            </div>
+
+            <div className="n-right">
+                
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
