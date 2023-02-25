fbd0d65ee0cca02a32aad8056beeed84cbfa47e9
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-02-25 13:18:39 +0000

    Update

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -3,7 +3,38 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <svg
+        id="eHGcy46pMvO1"
+        className="svg_1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 300 300"
+        shapeRendering="geometricPrecision"
+        textRendering="geometricPrecision"
+      >
+        <polygon
+          points="0,-222.00217 211.13661,-68.602443 130.489602,179.603528 -130.489602,179.603528 -211.13661,-68.602443 0,-222.00217"
+          transform="matrix(-.59518-1.395237 1.392004-.593802-102.420419 251.196233)"
+          fill="#004aad"
+          strokeWidth="0"
+        />
+      </svg>
+      <svg
+        id="eEnxzOBr7p61"
+        className="svg_2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 300 300"
+        shapeRendering="geometricPrecision"
+        textRendering="geometricPrecision"
+      >
+        <polygon
+          points="0,-222.00217 211.13661,-68.602443 130.489602,179.603528 -130.489602,179.603528 -211.13661,-68.602443 0,-222.00217"
+          transform="matrix(-.243321-1.497237 1.493769-.242758 398.201802-110.642364)"
+          fill="#004aad"
+          strokeWidth="0"
+        />
+      </svg>
+
+      <div className="img"></div>
     </div>
   );
 }
