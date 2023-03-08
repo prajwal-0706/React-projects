4fb8d2994002e4eb0240fabdea41602d2ea3981b
prajwal-0706 <117188729+prajwal-0706@users.noreply.github.com>
2023-03-08 17:46:52 +0000

    update

--- e-commerce-site/src/components/Swiper/SwiperImage.jsx
+++ e-commerce-site/src/components/Swiper/SwiperImage.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import "./SwiperImage.css";
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const SwiperImage = () => {
   return (
     <div className="image-slider">
-      <Swiper>
-        <SwiperSlide>Slide-1</SwiperSlide>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+      >
+        <SwiperSlide>
+          <img src="https://www.pexels.com/photo/green-tree-268533/" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>Slide-2</SwiperSlide>
+        <SwiperSlide>Slide-3</SwiperSlide>
+        <SwiperSlide>Slide-4</SwiperSlide>
+        <SwiperSlide>Slide-5</SwiperSlide>
+        <SwiperSlide>Slide-6</SwiperSlide>
+        <SwiperSlide>Slide-7</SwiperSlide>
+        <SwiperSlide>Slide-8</SwiperSlide>
       </Swiper>
     </div>
   );
