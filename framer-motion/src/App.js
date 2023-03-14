1ccd08912aa9b8fef802fbbfc3187bd2dfd47e59
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-03-14 23:46:28 +0530

    Framer Motion Start

--- framer-motion/src/App.js
+++ framer-motion/src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
+import { motion } from 'framer-motion';
 
 function App() {
+  const container = {
+    hidden: {
+      opacity: 0,
+    },
+
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.4,
+      },
+    },
+  };
+
+  const item = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+    },
+  };
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <motion.div className="App">
+      <motion.h1>Hello World</motion.h1>
+
+      <motion.ul animate="show" initial="hidden" variants={container}>
+        <motion.li variants={item}>Jidnyasa</motion.li>
+        <motion.li variants={item}>Jidnisa</motion.li>
+        <motion.li variants={item}>Khushi</motion.li>
+        <motion.li variants={item}>Catty</motion.li>
+      </motion.ul>
+    </motion.div>
   );
 }
 
