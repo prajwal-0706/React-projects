4edbdcb705e1e7b1a674502865c94885cac437ec
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-04-04 18:15:28 +0530

    Update

--- sudoku-game/src/components/Board/Board.jsx
+++ sudoku-game/src/components/Board/Board.jsx
@@ -1,24 +1,373 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Board.css';
 import Input from '../Inputs/Input';
 
 const Board = () => {
-  const boardNumbers = [
-    [2, 0, 0, 0, 0, 0, 0, 6, 0],
-    [0, 7, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 4, 0, 0, 0, 0],
-    [0, 0, 0, 0, 6, 0, 0, 0, 0],
-    [0, 0, 0, 0, 8, 0, 7, 0, 0],
-    [0, 4, 0, 0, 8, 0, 0, 0, 5],
-    [0, 0, 6, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 7, 0, 0, 0, 0, 0],
-    [9, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
+  const [boardNumbers, setboardNumbers] = useState([
+    [
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+    ],
+    [
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+    ],
+    [
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+    ],
+    [
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+    ],
+    [
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+    ],
+    [
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+    ],
+    [
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+    ],
+    [
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+    ],
+    [
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 0,
+        active: false,
+      },
+      {
+        data: 1,
+        active: true,
+      },
+    ],
+  ]);
+
+  const clickHandler = (row, col) => {
+    // setboardNumbers((prev) => {
+    //   return [...prev];
+    //   // boardNumbers[row][col].active;
+    // });
+    console.log(boardNumbers[row][col].data);
+    console.log(boardNumbers[row][col].active);
+  };
 
   return (
     <div className="mainBoard">
       <div className="heading">
         <h1>Sudoku</h1>
+        <div
+          style={{
+            height: '90%',
+            width: '50%',
+          }}
+          className="Numbers"
+        ></div>
       </div>
       <div className="board">
         {boardNumbers.map((board_input, index) => {
@@ -35,6 +384,9 @@ const Board = () => {
               {board_input.map((board, id) => {
                 return (
                   <Input
+                    clickHandler={clickHandler}
+                    row={index}
+                    col={id}
                     val={board}
                     index={id + 1}
                     key={Math.floor(Math.random() * 1000 + 3)}
--- sudoku-game/src/components/Inputs/Input.jsx
+++ sudoku-game/src/components/Inputs/Input.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Input.css';
 
-const Input = ({ val, index }) => {
-  const clickHandler = () => {
-    alert('Hiii');
-  };
-
+const Input = ({ val, index, row, col, clickHandler }) => {
   return (
     <div
-      onClick={clickHandler}
+      onClick={() => {
+        clickHandler(row, col);
+      }}
       type="text"
       value={val}
-      className={`boardInput ${
-        index !== 9 ? (index % 3 === 0 ? 'style-for-col' : '') : ''
-      }`}
+      className={`boardInput 
+       ${index !== 9 ? (index % 3 === 0 ? 'style-for-col' : '') : ''}`}
     />
   );
 };
