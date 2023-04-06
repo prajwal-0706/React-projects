4e7ec144b0aaed90ba76dee0dffa774b96fbf7d9
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-04-06 20:55:08 +0530

    Created Num Component

--- sudoku-game/src/components/Board/Board.jsx
+++ sudoku-game/src/components/Board/Board.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Board.css';
 import Input from '../Inputs/Input';
+import NumberInput from '../NumberInput/NumberInput';
 
 const Board = () => {
   const [boardNumbers, setboardNumbers] = useState([
@@ -349,52 +350,47 @@ const Board = () => {
   ]);
 
   const clickHandler = (row, col) => {
-    // setboardNumbers((prev) => {
-    //   return [...prev];
-    // });
     console.log(boardNumbers[row][col].data);
     console.log(boardNumbers[row][col].active);
   };
 
   return (
     <div className="mainBoard">
-      <div className="heading">
-        <h1>Sudoku</h1>
-        <div
-          style={{
-            height: '90%',
-            width: '50%',
-          }}
-          className="Numbers"
-        ></div>
+      <div className="left">
+        <div className="heading">
+          <h1>Sudoku</h1>
+        </div>
+        <NumberInput />
       </div>
-      <div className="board">
-        {boardNumbers.map((board_input, index) => {
-          return (
-            <div
-              className={`row ${
-                index + 1 !== 9
-                  ? (index + 1) % 3 === 0
-                    ? 'style-for-row'
+      <div className="right">
+        <div className="board">
+          {boardNumbers.map((board_input, index) => {
+            return (
+              <div
+                className={`row ${
+                  index + 1 !== 9
+                    ? (index + 1) % 3 === 0
+                      ? 'style-for-row'
+                      : ''
                     : ''
-                  : ''
-              }`}
-            >
-              {board_input.map((board, id) => {
-                return (
-                  <Input
-                    clickHandler={clickHandler}
-                    row={index}
-                    col={id}
-                    val={board}
-                    index={id + 1}
-                    key={Math.floor(Math.random() * 1000 + 3)}
-                  />
-                );
-              })}
-            </div>
-          );
-        })}
+                }`}
+              >
+                {board_input.map((board, id) => {
+                  return (
+                    <Input
+                      clickHandler={clickHandler}
+                      row={index}
+                      col={id}
+                      val={board}
+                      index={id + 1}
+                      key={Math.floor(Math.random() * 1000 + 3)}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
---
+++ sudoku-game/src/components/NumberInput/NumberInput.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './NumberInput.css';
+
+const NumberInput = () => {
+  return (
+    <div className="board Container">
+      <div className="Numbers">
+        <div className="boardInput numbers">1</div>
+        <div className="boardInput numbers">2</div>
+        <div className="boardInput numbers">3</div>
+        <div className="boardInput numbers">4</div>
+        <div className="boardInput numbers">5</div>
+        <div className="boardInput numbers">6</div>
+        <div className="boardInput numbers">7</div>
+        <div className="boardInput numbers">8</div>
+        <div className="boardInput numbers">9</div>
+      </div>
+    </div>
+  );
+};
+
+export default NumberInput;
