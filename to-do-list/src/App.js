1053b2c2a37919a5f77fdfe357984585cf4eaa8a
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-04-07 21:35:49 +0530

    update

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,34 +1,34 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import NoToDo from "./components/no-to-do/NoToDo";
-import ToDoList from "./components/toDoList/ToDoList";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import NoToDo from './components/no-to-do/NoToDo';
+import ToDoList from './components/toDoList/ToDoList';
 
 const App = () => {
-  const classFromStorage = localStorage.getItem("ClassDecider");
-  const noteFromLocalStorage = localStorage.getItem("items");
-  const promptFromLocalStorage = localStorage.getItem("Prompt");
+  const classFromStorage = localStorage.getItem('ClassDecider');
+  const noteFromLocalStorage = localStorage.getItem('items');
+  const promptFromLocalStorage = localStorage.getItem('Prompt');
 
   const [ClassDecider, setClassDecider] = useState(
     () => JSON.parse(classFromStorage) || false
   );
 
-  const [note, setnote] = useState("Prajwal");
+  const [note, setnote] = useState('Prajwal');
 
   const [toDoList, settoDoList] = useState(
     () => JSON.parse(noteFromLocalStorage) || []
   );
 
   const [Prompt, setPrompt] = useState(
-    () => JSON.parse(promptFromLocalStorage) || ""
+    () => JSON.parse(promptFromLocalStorage) || ''
   );
 
   useEffect(() => {
     if (toDoList.length === 0) {
       setClassDecider(false);
     }
-    localStorage.setItem("items", JSON.stringify(toDoList));
-    localStorage.setItem("ClassDecider", JSON.stringify(ClassDecider));
-    localStorage.setItem("Prompt", JSON.stringify(Prompt));
+    localStorage.setItem('items', JSON.stringify(toDoList));
+    localStorage.setItem('ClassDecider', JSON.stringify(ClassDecider));
+    localStorage.setItem('Prompt', JSON.stringify(Prompt));
   }, [toDoList]);
 
   const handleChange = (e) => {
@@ -45,16 +45,16 @@ const App = () => {
       },
     ]);
 
-    setnote("");
+    setnote('');
   };
 
   const createClickHandler = () => {
     setClassDecider(!ClassDecider);
-    setPrompt(prompt("Enter the Name For Your To Do List :-"));
+    setPrompt(prompt('Enter the Name For Your To Do List :-'));
     createNewNote();
   };
   return (
-    <div className={`App ${ClassDecider && "click"}`}>
+    <div className={`App ${ClassDecider && 'click'}`}>
       {toDoList.length > 0 ? (
         <ToDoList
           toDoList={toDoList}
