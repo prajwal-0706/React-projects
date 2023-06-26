b943652e0a6b685812c153ef646d650865ff0489
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-06-26 17:24:12 +0530

    Update

--- sudoku-game/src/components/Timer/Timer.jsx
+++ sudoku-game/src/components/Timer/Timer.jsx
@@ -1,22 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Timer.css';
 const Timer = () => {
-  const [timer, setTimer] = useState(false);
-  const [count, setCount] = useState(0);
-
-
-
-  useEffect(() => {}, []);
-
-  return (
-    <div className="timer">
-      <span className="time hr">00</span>
-      <span className="colon">:</span>
-      <span className="time min">00</span>
-      <span className="colon">:</span>
-      <span className="time sec">00</span>
-    </div>
-  );
+  return <div className="hewelo">hello</div>;
 };
 
 export default Timer;
