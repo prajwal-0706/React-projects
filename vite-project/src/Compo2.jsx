84d023c06795b9c3f002823090658d6934c2354b
prajwal-0706 <prajwalgadhave356@gmail.com>
2023-12-27 00:12:35 +0530

    fix: Changes in turborepo

--- vite-project/src/Compo2.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Compo2() {
-  return <div>Compo2</div>;
-}
--- vite-project/src/Component.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-export default function Element() {
-  return <Link to={'/element'}>Component</Link>;
-}
